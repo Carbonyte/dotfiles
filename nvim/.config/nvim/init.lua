aa31700d429392f38226aea909e38972d9bb2fe3
Joshua Staub <408242-Jode@users.noreply.gitlab.com>
2025-01-30 21:04:14 -0500

    Add treesitter and telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -21,10 +21,37 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set smartindent")
+
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		{"loctvl842/monokai-pro.nvim"}
+		{"loctvl842/monokai-pro.nvim"},
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.8",
+			dependencies = {"nvim-lua/plenary.nvim"}
+		},
+		{
+			"nvim-treesitter/nvim-treesitter",
+			build = ":TSUpdate",
+			config = function() 
+				local configs = require("nvim-treesitter.configs")
+
+				configs.setup({
+				ensure_installed = {
+					"c", "lua", "vim", "vimdoc", "query", "javascript", "html",
+					"cpp", "rust"
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },  
+			})
+		end
+		}
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
@@ -34,10 +61,14 @@ require("lazy").setup({
 })
 
 require("monokai-pro").setup()
+require("telescope").setup()
 vim.cmd("colorscheme monokai-pro-classic")
 
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set smartindent")
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {desc = "Telescope find files"})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Telescope live grep"})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help tags'})
+
+
 
